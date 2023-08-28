f0a1166a500e8c3483fbadddb8707ed388b00a22
Damokless <damokless@protonmail.com>
2023-08-28 03:19:34 +0200

    feat : page playlist manager

--- src/app/playlist-manager/page.tsx
+++ src/app/playlist-manager/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 import Navbar from "@/components/navbar/Navbar";
+import Image from "next/image";
 import { useState } from "react";
+interface Channel {
+  url: string;
+  tvgLogo: string;
+  tvgId: string;
+  name: string;
+}
 
 export default function PlaylistManager() {
   const [channels, setChannels] = useState([]);
@@ -31,6 +38,31 @@ export default function PlaylistManager() {
           <input type="file" className="hidden" accept=".m3u,.m3u8" onChange={(e) => handleFile(e.target.files)} />
         </label>
       </div>
+      {channels.length > 0 && (
+      <>
+        <div className="grid grid-cols-4 gap-4 bg-gray-100">
+          {channels.map((channel: Channel) => (
+            <div className="max-w-lg mx-auto my-10 w-4/5	 bg-gray-50 border border-blue-700 rounded-lg shadow-lg p-5" key={channel.url}>
+              {channel.tvgLogo ? <Image className="w-32 h-32  mx-auto" width={128} height={128} src={channel.tvgLogo} alt="Tv logo" /> : <div className="w-32 h-32"></div>}
+              <h2 className="text-center text-1xl font-semibold mt-3">{channel.tvgId}</h2>
+              <p className="text-center text-gray-600 mt-1">{channel.name}</p>
+              <div className="flex justify-center mt-5">
+                <div className="mt-8 flex flex-wrap justify-center gap-4">
+                  <button className="block w-full rounded bg-white border border-red-700 shadow-lg px-12 py-3 text-sm font-medium text-black hover:bg-red-500 hover:text-white focus:outline-none focus:ring-none sm:w-auto">
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className=" flex flex-wrap justify-center gap-4 bg-gray-100">
+          <a className="block w-full rounded bg-white border border-green-700 shadow-lg px-12 py-3 text-sm font-medium text-black hover:bg-green-500 hover:text-white focus:outline-none focus:ring-none sm:w-auto" href="/playlist-manager">
+            Save playlist
+          </a>
+        </div>
+      </>
+      )}
     </div>
   );
 }
